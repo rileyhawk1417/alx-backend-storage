51ccd88230b3e5956ec259e660699762d5c29b54
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-24 15:43:42 +0200

    fix: not null for primary key

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Create users table and add country as a column
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     email VARCHAR(255) NOT NULL UNIQUE, 
     name VARCHAR(255), 
     country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'));
