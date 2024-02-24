670e4b6c6efbd8d1ee9b408b9eef94606c421036
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-24 16:21:52 +0200

    refactor: revert code to previous state

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,22 +1,16 @@
 -- Calculate bonus marks for students
-DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
-BEGIN
-    DECLARE project_count INT DEFAULT 0;
-    DECLARE project_id INT DEFAULT 0;
-    SELECT COUNT(id) INTO project_id 
-    FROM projects WHERE name =  project_name;
 
-    IF project_count = 0 THEN
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    SELECT id INTO project_id FROM projects WHERE name =  project_name;
+    
+    IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
     END IF;
 
-    SELECT id INTO projects_id
-    FROM projects
-    WHERE name = project_name;
-
-    INSERT INTO corrections(user_id, project_id, score)
-    VALUES (user_id, project_id, score)
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END $$
 DELIMITER;
