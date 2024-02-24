ff6c325744a3a96ae535d19f0021db00cff76dda
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-24 15:47:52 +0200

    fix: rework table attributes

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,7 @@
 -- Create users table and add country as a column
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     email VARCHAR(255) NOT NULL UNIQUE, 
     name VARCHAR(255), 
-    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'));
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
