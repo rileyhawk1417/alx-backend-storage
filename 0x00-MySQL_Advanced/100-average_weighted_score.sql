88f3359a575c48810ca8ff03a33eb1fa36db7464
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-24 16:46:12 +0200

    chore: complete advanced tasks

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,16 @@
+-- Calculate the average weighted score for the student
+
+DROP PROCEDURE IF EXISTS ComputerAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputerAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT IF(SUM(score * weight) > 0, SUM(score * weight) / SUM(weight), 0)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END $$
+DELIMITER;
