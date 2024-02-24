3a5665bb195975b95a3e2642cff8e32e3c37cf34
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-24 15:46:09 +0200

    fix: remove drop table if exists condition

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,6 @@
 -- Create users table and add country as a column
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     email VARCHAR(255) NOT NULL UNIQUE, 
     name VARCHAR(255), 
     country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'));
